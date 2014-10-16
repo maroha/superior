b31d730ac01c62b24d4449d626583105602a9c7d
Kevin Nemeržitski <kevin.nemerzitski@gmail.com>
2014-10-16 20:53:44 +0300

    Warehouse: adding new products. done

--- src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
+++ src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
@@ -1,18 +1,23 @@
 package ee.ut.math.tvt.salessystem.ui.tabs;
 
 import javafx.collections.ListChangeListener;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -36,9 +41,10 @@ public class StockTab extends Tab {
 		panel.setBorder(new Border(new BorderStroke(Color.BLACK,
 				BorderStrokeStyle.SOLID, CornerRadii.EMPTY,
 				BorderWidths.DEFAULT)));
-
-		panel.add(drawStockMenuPane(), 0, 0);
-
+		Node n = drawStockMenuPane();
+		panel.add(n, 0, 0);
+		GridPane.setFillWidth(n, false);
+		
 		panel.add(drawStockMainPane(), 0, 1);
 		return panel;
 	}
@@ -50,12 +56,64 @@ public class StockTab extends Tab {
 		panel.setBorder(new Border(new BorderStroke(Color.BLACK,
 				BorderStrokeStyle.SOLID, CornerRadii.EMPTY,
 				BorderWidths.DEFAULT)));
-
+    	ColumnConstraints column2 = new ColumnConstraints();
+    	column2.setPrefWidth(75);
+    	panel.getColumnConstraints().addAll(new ColumnConstraints(), column2);
+		
+		
+		TextField idField = new TextField();
+		TextField nameField = new TextField();
+		TextField descField = new TextField();
+		TextField priceField = new TextField();
+		TextField quantityField = new TextField();
+		
+		
+		panel.add(new Label("Id"), 0, 0);
+		panel.add(new Label("Name"), 0, 1);
+		panel.add(new Label("Desc"), 0, 2);
+		panel.add(new Label("Price"), 0, 3);
+		panel.add(new Label("Quantity"), 0,4);
+		
+		panel.add(idField, 1, 0);
+		panel.add(nameField, 1, 1);
+		panel.add(descField, 1, 2);
+		panel.add(priceField, 1, 3);
+		panel.add(quantityField, 1,4);
+		
 		addItem = new Button("Add");
+		
+		addItem.setOnAction(new EventHandler<ActionEvent>(){
 
-		panel.add(addItem, 0, 0);
+			@Override
+			public void handle(ActionEvent event) {
+				long id = 0;
+				String name = null;
+				String desc = null;
+				double price = 0;
+				int quantity = 0;
+				try {
+					id = Long.parseLong(idField.getText());
+					name = nameField.getText();
+					desc = descField.getText();
+					price = Double.parseDouble(priceField.getText());
+					quantity = Integer.parseInt(quantityField.getText());
+				} catch (Exception e) {
+					return;
+				}
+				
+				StockItem item = new StockItem(
+						id, 
+						name, 
+						desc, 
+						price, quantity);
+				model.getWarehouseTableModel().addItem(item);
+			}});
 
-		return panel;
+		panel.add(addItem, 0,40);
+
+		TitledPane titledPane = new TitledPane("Add products", panel);
+		titledPane.setCollapsible(false);
+		return titledPane;
 	}
 
 	// table of the wareshouse stock
